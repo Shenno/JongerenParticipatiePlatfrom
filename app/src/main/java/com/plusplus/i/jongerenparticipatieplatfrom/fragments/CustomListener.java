039bb985c3cc11d0841d931620de566cd3e778a0
Shenno <shenno.w@gmail.com>
2015-05-22 16:11:59 +0200

    Changes

--- app/src/main/java/com/plusplus/i/jongerenparticipatieplatfrom/fragments/CustomListener.java
+++ app/src/main/java/com/plusplus/i/jongerenparticipatieplatfrom/fragments/CustomListener.java
@@ -14,14 +14,10 @@ public class CustomListener implements OnItemSelectedListener {
     public void onItemSelected(AdapterView<?> parent, View view, int pos,
                                long id) {
 
-        Toast.makeText(parent.getContext(),
-                "On Item Select : \n" + parent.getItemAtPosition(pos).toString(),
-                Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
 
     }
 

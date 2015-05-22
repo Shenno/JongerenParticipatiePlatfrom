8ff682ce054cd36bb5d12a5652a519704be52a33
MaximV93 <maxim.vandebroeck@gmail.com>
2015-05-22 04:33:59 +0200

    Layout fixes change pw

--- app/src/main/java/com/plusplus/i/jongerenparticipatieplatfrom/fragments/UpdatePassWordFragment.java
+++ app/src/main/java/com/plusplus/i/jongerenparticipatieplatfrom/fragments/UpdatePassWordFragment.java
@@ -3,6 +3,7 @@ package com.plusplus.i.jongerenparticipatieplatfrom.fragments;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +14,8 @@ import android.widget.EditText;
 import com.plusplus.i.jongerenparticipatieplatfrom.R;
 
 public class UpdatePassWordFragment extends Fragment {
-    private Button button;
-    private EditText txtCurrentPassword, txtNewPassword, txtConfirmNewPassword;
+    private Button confirmChangePassword;
+    private EditText txtCurrentPassword, txtNewPassword, txtConfirmNewPassword,txtEditPassword;
 
 
     public UpdatePassWordFragment() {
@@ -25,12 +26,23 @@ public class UpdatePassWordFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         final View rootView = inflater.inflate(R.layout.fragment_change_password_screen, container, false);
+        Typeface opensans = Typeface.createFromAsset(rootView.getContext().getAssets(), "fonts/OpenSans-Regular.ttf");
+        Typeface roboto = Typeface.createFromAsset(rootView.getContext().getAssets(), "fonts/Roboto-Regular.ttf");
 
-        button = (Button) rootView.findViewById(R.id.btnConfirmPasswordChange);
+
+
+        confirmChangePassword = (Button) rootView.findViewById(R.id.btnConfirmPasswordChange);
+        confirmChangePassword.setTypeface(opensans);
         txtCurrentPassword = (EditText) rootView.findViewById(R.id.txtCurrentPassword);
+        txtCurrentPassword.setTypeface(opensans);
         txtNewPassword = (EditText) rootView.findViewById(R.id.txtNewPassword);
+        txtNewPassword.setTypeface(opensans);
         txtConfirmNewPassword = (EditText) rootView.findViewById(R.id.txtConfirmNewPassword);
-        button.setOnClickListener(new View.OnClickListener() {
+        txtConfirmNewPassword.setTypeface(opensans);
+        txtEditPassword = (EditText) rootView.findViewById(R.id.myProfile);
+        txtEditPassword.setTypeface(roboto);
+
+        confirmChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -43,16 +55,16 @@ public class UpdatePassWordFragment extends Fragment {
                 if (txtConfirmNewPassword.getText().length() == 0) {
                     txtConfirmNewPassword.setError(getString(R.string.EmptyTextFieldCannotBeEmpty));
                 }
-                if(!txtNewPassword.getText().equals(txtConfirmNewPassword.getText())){
-                    if (txtConfirmNewPassword.getText().length()==0){
+                if (!txtNewPassword.getText().equals(txtConfirmNewPassword.getText())) {
+                    if (txtConfirmNewPassword.getText().length() == 0) {
                         txtConfirmNewPassword.setError(getString(R.string.EmptyTextFieldCannotBeEmpty));
-                    }else{
+                    } else {
                         txtConfirmNewPassword.setError(getString(R.string.PasswordsMisMatch));
                     }
 
                 }
 
-                if (!(txtCurrentPassword.getText().length() == 0 || txtNewPassword.getText().length() == 0 || txtConfirmNewPassword.getText().length() == 0)){
+                if (!(txtCurrentPassword.getText().length() == 0 || txtNewPassword.getText().length() == 0 || txtConfirmNewPassword.getText().length() == 0)) {
                     Fragment frag = new MyProfileFragment();
                     FragmentManager fragMan = getFragmentManager();
                     FragmentTransaction fragTran = fragMan.beginTransaction();
